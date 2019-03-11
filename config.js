5f2dd70a85babeb0b3cbb6d4347c0d3639a4da8a
Dávid Eredics <mail@davideredics.com>
2019-03-11 19:57:25 +0100

    override configuration with custom config file

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
 const config = require('./config.json');
 
-const env = process.env.NODE_ENV || 'development';
+let confFile = {};
+
+if (process.env.confFile) {
+  confFile = JSON.parse(fs.readFileSync(process.env.confFile));
+}
+
+const env = process.env.NODE_ENV || confFile.env || 'development';
 
 if (!Object.prototype.hasOwnProperty.call(config, env)) {
   console.log('no config found for the provided NODE_ENV');
@@ -9,19 +16,34 @@ if (!Object.prototype.hasOwnProperty.call(config, env)) {
 
 module.exports = {
   env,
-  port: process.env.PORT || config[env].port,
-  host: process.env.HOST || config[env].host,
-  name: process.env.NAME || config[env].name,
-  certPath: process.env.cert_path || config[env].certPath,
-  cert: process.env.cert || config[env].cert,
-  key: process.env.key || config[env].key,
-  jwtSecret: process.env.jwtSecret || config[env].jwtSecret,
-  jwtKeyPath: process.env.jwtKeyPath || config[env].jwtKeyPath,
-  jwtKeyPrv: process.env.jwtKeyPrv || config[env].jwtKeyPrivate,
-  jwtKeyPub: process.env.jwtKeyPub || config[env].jwtKeyPublic,
-  allowHTTP1: process.env.allowHTTP1 || config[env].allowHTTP1,
-  MongoDBurl: process.env.MONGODB_URI || config[env].MongoDBurl,
-  dbName: process.env.DB_NAME || config[env].dbName,
-  openReg: process.env.openReg || config[env].openRegistration,
-  openReceive: process.env.openReceive || config[env].openReceive,
+  port: process.env.PORT
+    || confFile.port || config[env].port,
+  host: process.env.HOST
+    || confFile.host || config[env].host,
+  name: process.env.NAME
+    || confFile.name || config[env].name,
+  certPath: process.env.cert_path
+    || confFile.certPath || config[env].certPath,
+  cert: process.env.cert
+    || confFile.cert || config[env].cert,
+  key: process.env.key
+    || confFile.key || config[env].key,
+  jwtSecret: process.env.jwtSecret
+    || confFile.jwtSecret || config[env].jwtSecret,
+  jwtKeyPath: process.env.jwtKeyPath
+    || confFile.jwtKeyPath || config[env].jwtKeyPath,
+  jwtKeyPrv: process.env.jwtKeyPrivate
+    || confFile.jwtKeyPrivate || config[env].jwtKeyPrivate,
+  jwtKeyPub: process.env.jwtKeyPublic
+    || confFile.jwtKeyPublic || config[env].jwtKeyPublic,
+  allowHTTP1: process.env.allowHTTP1
+    || confFile.allowHTTP1 || config[env].allowHTTP1,
+  MongoDBurl: process.env.MONGODB_URI
+    || confFile.MongoDBurl || config[env].MongoDBurl,
+  dbName: process.env.DB_NAME
+    || confFile.dbName || config[env].dbName,
+  openReg: process.env.openReg
+    || confFile.openRegistration || config[env].openRegistration,
+  openReceive: process.env.openReceive
+    || confFile.openReceive || config[env].openReceive,
 };
